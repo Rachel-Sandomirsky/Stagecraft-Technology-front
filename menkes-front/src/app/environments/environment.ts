4f342f800977578ec178869a5b3ffbaecb5e85d2
Toriti Pass <toriti17@gmail.com>
2024-12-07 23:29:00 +0200

    url

--- menkes-front/src/app/environments/environment.ts
+++ menkes-front/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://menkes-2-back.onrender.com',
     apiKey: '2dfc36e6b58f2e5234b56deff290dae246a06dea3c419059d2a44ecb9abfea23',
 
   }; 
