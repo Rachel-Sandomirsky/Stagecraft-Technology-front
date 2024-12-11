2b9ce56e9f73000d91c8eca4474a9a1373e191d9
Toriti Pass <toriti17@gmail.com>
2024-12-12 00:43:37 +0200

    commit

--- menkes-front/src/app/services/course.service.ts
+++ menkes-front/src/app/services/course.service.ts
@@ -14,7 +14,7 @@ export class CourseService {
 
 // Function to fetch real courses
   getCourses(): Observable<Course[]> {
-    //console.log('API URL:', `${environment.apiUrl}/endpoint`);
+    console.log('API URL:', `https://menkes-2-back.onrender.com/courses`);
 
     return this.apiService.get<Course[]>(this.apiUrl);
   }
