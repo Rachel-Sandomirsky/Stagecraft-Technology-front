479412258c9a858357d162e8f416f506e89ece38
Toriti Pass <toriti17@gmail.com>
2024-12-13 01:18:43 +0200

    ccc

--- menkes-front/src/app/services/course.service.ts
+++ menkes-front/src/app/services/course.service.ts
@@ -13,13 +13,13 @@ export class CourseService {
 
 // Function to fetch real courses
   getCourses(): Observable<Course[]> {
-    (window as any).apiUrl = "https://menkes-2-back.onrender.com/courses";
+    (window as any).kkk = "https://menkes-2-back.onrender.com/courses";
     
-    console.log('גרסה 5');
-    console.log('API URL:', (window as any).apiUrl); // וודאי שכתובת זו מודפסת
+    console.log('גרסה 6');
+    console.log('API URL:', (window as any).kkk); // וודאי שכתובת זו מודפסת
 
 try{
-    return this.apiService.get<Course[]>((window as any).apiUrl);
+    return this.apiService.get<Course[]>((window as any).kkk);
   } catch (error) {
     console.error('Error fetching courses:', error);
 throw new Error
