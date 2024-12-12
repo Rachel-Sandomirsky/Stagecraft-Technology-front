cb3b5865ea65cc82fab0e4fe5c0976458c338751
Toriti Pass <toriti17@gmail.com>
2024-12-12 20:36:55 +0200

    commit

--- menkes-front/src/app/services/course.service.ts
+++ menkes-front/src/app/services/course.service.ts
@@ -14,7 +14,7 @@ export class CourseService {
 
 // Function to fetch real courses
   getCourses(): Observable<Course[]> {
-    console.log('גרסה 1);
+    console.log('גרסה 1');
 try{
     return this.apiService.get<Course[]>(this.apiUrl);
   } catch (error) {
