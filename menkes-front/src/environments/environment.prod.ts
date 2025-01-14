0d2557acb6deb4200ae7974e348ec37f4618dbf6
rachali <rsrsrs3262@gmail.com>
2025-01-14 12:35:16 +0200

    For render

--- menkes-front/src/environments/environment.prod.ts
+++ menkes-front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     //apiUrl: 'https://menkes-2-back.onrender.com',
-     apiUrl: ' http://localhost:3000',
+     apiUrl: 'https://menkes-2-back-1.onrender.com',
     apiKey: '2dfc36e6b58f2e5234b56deff290dae246a06dea3c419059d2a44ecb9abfea23',
   }; 
